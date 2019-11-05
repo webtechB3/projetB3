f229f37bdd6e7eff5afc8fa3df846c085458f79a
Jerem <jeremy.agrezio@orange.fr>
2019-11-05 20:09:06 +0100

    Spring API  added

---
+++ api/src/main/java/com/projetb3/api/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package com.projetb3.api.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "hello world";
+    }
+}
